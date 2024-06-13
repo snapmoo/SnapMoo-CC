ceed4c3dfd4e1c3ee7c5a81da78f8b95231e2c14
snapmoo <snapmoobangkit@gmail.com>
2024-06-14 01:56:30 +0700

    Update articleRoutes.js

--- src/routes/articleRoutes.js
+++ src/routes/articleRoutes.js
@@ -1,13 +1,33 @@
-// src/routes/articleRoutes.js
+// articleRoutes.js
+
 const express = require('express');
 const router = express.Router();
 const db = require('../config/firestore');
+
+// Helper function to convert Firestore Timestamp to readable date
+function convertTimestampToReadableDate(timestamp) {
+    const date = timestamp.toDate();
+    return date.toLocaleString('en-US', { timeZone: 'UTC', year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' });
+}
 
 // GET all articles
 router.get('/articles', async (req, res) => {
     try {
         const articlesSnapshot = await db.collection('articles').get();
-        const articles = articlesSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+        if (articlesSnapshot.empty) {
+            res.status(404).json({ message: 'No articles found.' });
+            return;
+        }
+
+        const articles = [];
+        articlesSnapshot.forEach(doc => {
+            const article = doc.data();
+            if (article.date) {
+                article.date = convertTimestampToReadableDate(article.date);
+            }
+            articles.push({ id: doc.id, ...article });
+        });
+
         res.status(200).json({ message: 'Articles retrieved successfully.', data: articles });
     } catch (error) {
         console.error(error);
