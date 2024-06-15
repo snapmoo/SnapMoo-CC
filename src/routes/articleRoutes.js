7ce423bd5e863f783548dc0e60c614c8fd144e6c
losinanda <kadekprawira29@gmail.com>
2024-06-15 20:14:15 +0800

    add get report by id and search article by query

--- src/routes/articleRoutes.js
+++ src/routes/articleRoutes.js
@@ -2,23 +2,20 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../config/firestore');
-const moment = require('moment');
-
-function convertTimestampToReadableDate(timestamp) {
-    const seconds = timestamp._seconds;
-    const nanoseconds = timestamp._nanoseconds;
-
-    // Buat objek Date berdasarkan seconds dan nanoseconds
-    const date = new Date(seconds * 1000 + nanoseconds / 1000000);
-
-    // Format tanggal menggunakan Moment.js
-    return moment(date).format('MMMM DD, YYYY [at] h:mm:ss.SSS A [UTC]Z');
-}
+const authMiddleware = require('../middleware/auth');
 
 // GET all articles
-router.get('/articles', async (req, res) => {
+router.get('/articles', authMiddleware, async (req, res) => {
     try {
-        const articlesSnapshot = await db.collection('articles').get();
+        const query = req.query.q;
+        let articlesRef = db.collection('articles');
+
+        if (query) {
+            articlesRef = articlesRef.where('title', '>=', query).where('title', '<=', query + '\uf8ff');
+        }
+
+        const articlesSnapshot = await articlesRef.get();
+
         if (articlesSnapshot.empty) {
             res.status(404).json({ message: 'No articles found.' });
             return;
@@ -27,15 +24,12 @@ router.get('/articles', async (req, res) => {
         const articles = [];
         articlesSnapshot.forEach(doc => {
             const article = doc.data();
-            if (article.date) {
-                article.date = convertTimestampToReadableDate(article.date);
-            }
             articles.push({ id: doc.id, ...article });
         });
 
         res.status(200).json({ message: 'Articles retrieved successfully.', data: articles });
     } catch (error) {
-        console.error(error);
+        // console.error(error);
         res.status(500).json({ message: 'An unexpected error occurred. Please try again later.' });
     }
 });
