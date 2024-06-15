712c5b0afc10ce8c99b663947661db65c2f4efff
losinanda <kadekprawira29@gmail.com>
2024-06-15 20:37:12 +0800

    edit article route

--- src/routes/articleRoutes.js
+++ src/routes/articleRoutes.js
@@ -2,10 +2,32 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../config/firestore');
-const authMiddleware = require('../middleware/auth');
+
 
 // GET all articles
-router.get('/articles', authMiddleware, async (req, res) => {
+router.get('/articles', async (req, res) => {
+    try {
+        const articlesSnapshot = await db.collection('articles').get();
+        if (articlesSnapshot.empty) {
+            res.status(404).json({ message: 'No articles found.' });
+            return;
+        }
+
+        const articles = [];
+        articlesSnapshot.forEach(doc => {
+            const article = doc.data();
+            articles.push({ id: doc.id, ...article });
+        });
+
+        res.status(200).json({ message: 'Articles retrieved successfully.', data: articles });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'An unexpected error occurred. Please try again later.' });
+    }
+});
+
+// Search articles
+router.get('/articles/search', async (req, res) => {
     try {
         const query = req.query.q;
         let articlesRef = db.collection('articles');
@@ -24,14 +46,18 @@ router.get('/articles', authMiddleware, async (req, res) => {
         const articles = [];
         articlesSnapshot.forEach(doc => {
             const article = doc.data();
+            if (article.date) {
+                article.date = convertTimestampToReadableDate(article.date);
+            }
             articles.push({ id: doc.id, ...article });
         });
 
         res.status(200).json({ message: 'Articles retrieved successfully.', data: articles });
     } catch (error) {
-        // console.error(error);
+        console.error(error);
         res.status(500).json({ message: 'An unexpected error occurred. Please try again later.' });
     }
 });
+
 
 module.exports = router;
